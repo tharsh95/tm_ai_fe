e9ba9ea5d7b88b71e2e1a6fd77a476c6b7fc3c7d
Harsh Tiwari <harsh@kimo.ai>
2025-04-05 20:54:33 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import AppRoutes from './AppRoutes.tsx'
 import { HashRouter } from 'react-router-dom'
-import { Hash } from 'lucide-react'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
